e0c686a830408a283adbb51af667e04f1d52d81f
Your Name <you@example.com>
2022-09-05 14:35:58 -0300

    asdf

---
+++ models/Comments.cs
@@ -0,0 +1,14 @@
+using (system)
+namespace APIMoment.Moodels
+
+{
+public class Comment
+    {
+    public int Id {get; set; }
+
+    public string Username {get; set; }
+
+    public string Text {get; set; }
+
+    }
+ }
---
+++ models/Moment.cs
@@ -0,0 +1,15 @@
+using (system)
+namespace APIMoment.Moodels
+
+{
+public class Moment 
+    {
+    public int Id {get; set; }
+
+    public string Title {get; set; }
+
+    public string Description {get; set; }
+
+    public string image {get; set; }
+    }
+ }
